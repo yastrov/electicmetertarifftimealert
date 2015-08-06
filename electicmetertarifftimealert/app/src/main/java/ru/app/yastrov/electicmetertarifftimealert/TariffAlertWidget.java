e59d80b005c7a97e655ea862c6417bc8e93b611c
Yuriy Astrov <yuriastrov@gmail.com>
2015-08-06 20:50:50 +0300

    Refactoring for Widget: Log, calls for super, Intent for start main Activity have been added. Removed unused function.

--- electicmetertarifftimealert/app/src/main/java/ru/app/yastrov/electicmetertarifftimealert/TariffAlertWidget.java
+++ electicmetertarifftimealert/app/src/main/java/ru/app/yastrov/electicmetertarifftimealert/TariffAlertWidget.java
@@ -8,6 +8,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.util.Calendar;
@@ -17,13 +18,16 @@ import java.util.Calendar;
  */
 public class TariffAlertWidget extends AppWidgetProvider {
     static final String UPDATE_ALL_WIDGETS = "update_all_widgets";
+    static  final String LOG_TAG = "TariffAlertWidget";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(LOG_TAG, "onUpdate");
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
-            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+            updateWidget(context, appWidgetManager, appWidgetIds[i]);
         }
     }
 
@@ -31,25 +35,17 @@ public class TariffAlertWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        super.onEnabled(context);
+        Log.d(LOG_TAG, "onEnabled");
         SetAlarm(context);
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context);
+        Log.d(LOG_TAG, "onDisabled");
         CancelAlarm(context);
-    }
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.tariff_alert_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     /**
@@ -61,12 +57,14 @@ public class TariffAlertWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+        Log.d(LOG_TAG, "onReceive");
         if (intent.getAction().equalsIgnoreCase(UPDATE_ALL_WIDGETS)) {
             ComponentName thisAppWidget = new ComponentName(
                     context.getPackageName(), getClass().getName());
             AppWidgetManager appWidgetManager = AppWidgetManager
                     .getInstance(context);
             int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            // It's reason if widgets 1 or 2.
             for (int appWidgetID : ids) {
                 updateWidget(context, appWidgetManager, appWidgetID);
             }
@@ -81,6 +79,7 @@ public class TariffAlertWidget extends AppWidgetProvider {
      */
     static void updateWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
+        Log.d(LOG_TAG, "updateWidget");
         int color = Color.WHITE;
         int result = AlarmHelper.WhatTariffNow(context);
         CharSequence widgetText = context.getString(R.string.app_name);
@@ -100,11 +99,22 @@ public class TariffAlertWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.tariff_alert_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
         views.setInt(R.id.appwidget_text, "setBackgroundColor", color);
+        // Open MainActivity
+        Intent configIntent = new Intent(context, MainActivity.class);
+        configIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        /* PendingIntent.getActivity() for call Activity
+            or PendingIntent.getBroadcast() for Widget or BroadcastReceiver
+         */
+        PendingIntent pIntent = PendingIntent.getActivity(context, appWidgetId, configIntent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pIntent);
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     private void SetAlarm(Context context) {
+        Log.d(LOG_TAG, "SetAlarm");
         AlarmManager alarmMgr;
         PendingIntent alarmIntent;
         alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
@@ -122,6 +132,7 @@ public class TariffAlertWidget extends AppWidgetProvider {
     }
 
     private void CancelAlarm(Context context) {
+        Log.d(LOG_TAG, "CancelAlarm");
         AlarmManager alarmMgr;
         PendingIntent alarmIntent;
         alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
