52e15440b4eecd56d9043984de750610e8681a4e
Yuriy Astrov <yuriastrov@gmail.com>
2015-08-24 15:27:16 +0300

    Notification helper: Now open MainActivity if onClick on Notification.

--- electicmetertarifftimealert/app/src/main/java/ru/app/yastrov/electicmetertarifftimealert/MyNotificationHelper.java
+++ electicmetertarifftimealert/app/src/main/java/ru/app/yastrov/electicmetertarifftimealert/MyNotificationHelper.java
@@ -1,8 +1,10 @@
 package ru.app.yastrov.electicmetertarifftimealert;
 
 import android.app.Notification.Builder;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.app.NotificationManager;
+import android.content.Intent;
 
 /**
  * Created by Yuriy Astrov on 28.07.2015.
@@ -12,14 +14,18 @@ public class MyNotificationHelper {
 
     public static void MakeNotify(Context context, String title, String content)
     {
+        Intent configIntent = new Intent(context, MainActivity.class);
+        configIntent.setAction(Long.toString(System.currentTimeMillis()));
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+
         Builder builder = new Builder(context)
                 //.setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.drawable.ic_stat_name)
                 //.setVisibility(Notification.VISIBILITY_PUBLIC)
                 .setAutoCancel(true)
                 .setContentTitle(title)
-                .setContentText(content);
-        //.setContentIntent(getPendingIntent())
+                .setContentText(content)
+                .setContentIntent(pIntent);
         NotificationManager manager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(NOTIFY_ID, builder.build());
